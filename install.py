a9d640fbc5f39275b91a9a7d8a7004ed88594ee9
Matei Bianca <Bianca@Mateis-MacBook-Air.local>
2017-04-26 22:37:36 -0400

    implementing arguments

--- install.py
+++ install.py
@@ -2,11 +2,23 @@
 
 import os
 import subprocess
+import argparse
+import sys
 
-pkg = input("what package would you like to install?\n")
 
-print("Installing %s..." % pkg)
-subprocess.check_call(['apt-get', '-y', 'install', pkg], stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
+# Override the default behavior of the error method
+class BSParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+parser = BSParser(description='Install packages')
+parser.add_argument('--pkg', '-p', action='store_true', type=str)
+args = parser.parse_args()
+
+print("Installing %s..." % args.pkg)
+subprocess.check_call(['apt-get', '-y', 'install', args.pkg], stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
 print("Installation done.")
 print("Updating...")
 subprocess.check_call(['apt-get', '-y', 'update'], stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
