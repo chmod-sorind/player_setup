4752779d96c1fbc0b14390f4d8392324727fb168
Matei Bianca <Bianca@Mateis-MacBook-Air.local>
2017-04-26 21:35:03 -0400

    added update and upgrade lines

--- install.py
+++ install.py
@@ -3,4 +3,8 @@
 import os
 import subprocess
 
-subprocess.check_call(['apt-get', 'install', 'htoo'], stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
+subprocess.check_call(['apt-get', 'install', 'htop'], stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
+subprocess.check_call(['apt-get', 'update'], stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
+subprocess.check_call(['apt-get', 'upgrade'], stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
+subprocess.check_call(['apt-get', 'autoremove'], stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
+subprocess.check_call(['apt-get', 'autoclean'], stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
